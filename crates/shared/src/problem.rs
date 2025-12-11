b501e547c98232cfa1f3b2ad7ec33bb52e813106
Dangerise <dangerise@qq.com>
2025-12-11 16:39:05 +0800

    Update problem.rs

--- crates/shared/src/problem.rs
+++ crates/shared/src/problem.rs
@@ -1,14 +1,22 @@
 use super::*;
+use compact_str::CompactString;
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
-pub struct Pid(pub String);
+pub struct Pid(pub CompactString);
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct ProblemFront {
-    #[serde(default)]
     pub pid: Pid,
     pub title: String,
     pub statement: String,
+    pub time_limit: f32,
+    pub memory_limit: u32,
+}
+
+impl Pid {
+    pub fn new(s: &str) -> Self {
+        Self(CompactString::new(s))
+    }
 }
 
 impl std::fmt::Display for Pid {
@@ -20,6 +28,6 @@ impl std::fmt::Display for Pid {
 impl std::str::FromStr for Pid {
     type Err = u8;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Pid(s.to_string()))
+        Ok(Pid(CompactString::new(s)))
     }
 }
