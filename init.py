4ebf5f632e56e92ac816974747e5a1eb771626cb
Dangerise <dangerise@qq.com>
2026-01-21 09:13:25 +0800

    update

--- init.py
+++ init.py
@@ -1,9 +1,13 @@
+import os
 import sqlite3
 
-con = sqlite3.connect("/home/dangerise/mygoj/data.db")
+os.makedirs("tmp", exist_ok=True)
+
+con = sqlite3.connect("./tmp/empty.db")
 
 with open("crates/server/src/sql/create.sql") as f:
     sql = f.read()
     con.executescript(sql)
+
 con.commit()
 con.close()
